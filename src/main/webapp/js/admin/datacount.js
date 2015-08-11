16af506c9817722a57bb326c669b9a985cd982cb
ligson <593949938@qq.com>
2015-08-11 12:19:27 +0000

    qqqq

---
+++ src/main/webapp/js/admin/datacount.js
@@ -0,0 +1,37 @@
+/**
+ * Created by ligson on 2015/8/11.
+ */
+$(function () {
+    $('#container').highcharts({
+        title: {
+            text: '资源点播次数统计',
+            x: -20 //center
+        },
+        xAxis: {
+            categories: ['资源1','资源1','资源1','资源1','资源1','资源1','资源1','资源1','资源1','资源1']
+        },
+        yAxis: {
+            title: {
+                text: '点播次数'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '次'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [ {
+            name: '点播次数',
+            data: [100, 200,11,44,1411,44,11,4774,1414,7474,441,7474,88,4488,414,36365,525]
+        }]
+    });
+});
