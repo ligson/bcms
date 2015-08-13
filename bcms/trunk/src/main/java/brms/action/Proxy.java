aebc3d35fdf5414cc2fc73c0fe0685e305dbeb5d
ligson <ligson@363c9ccc-0a61-4707-9bf7-43c13cfca925>
2015-08-13 02:41:40 +0000

    统计

--- bcms/trunk/src/main/java/brms/action/Proxy.java
+++ bcms/trunk/src/main/java/brms/action/Proxy.java
@@ -15,9 +15,7 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.*;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -68,7 +66,9 @@ public class Proxy extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response) throws ServletException, IOException {
-        doPost(request, response);
+        String uri = parseToUrl(request);
+        HttpGet get = new HttpGet(uri);
+        executeMethod(get, response);
     }
 
     /**
@@ -78,20 +78,55 @@ public class Proxy extends HttpServlet {
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response) throws ServletException, IOException {
         String url = request.getParameter("url");
-        Map<String, Object> result = new HashMap<String, Object>();
         HttpPost httpPost = new HttpPost(BASE_URL + url);
-        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-        Enumeration enumeration = request.getParameterNames();
-        while (enumeration.hasMoreElements()) {
-            String name = enumeration.nextElement().toString();
-            String value = request.getParameter(name);
-            NameValuePair nameValuePair = new BasicNameValuePair(name, value);
-            nameValuePairs.add(nameValuePair);
-        }
-        HttpEntity httpEntity = new UrlEncodedFormEntity(nameValuePairs);
-        httpPost.setEntity(httpEntity);
-        CloseableHttpResponse httpResponse = (CloseableHttpResponse) httpClient.execute(httpPost, context);
+        HttpEntity entity = parseToEntity(request);
+        httpPost.setEntity(entity);
+        executeMethod(httpPost, response);
+    }
 
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String url = request.getParameter("url");
+        HttpPut httpPut = new HttpPut(BASE_URL + url);
+        HttpEntity entity = parseToEntity(request);
+        httpPut.setEntity(entity);
+        executeMethod(httpPut, response);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String uri = parseToUrl(request);
+        HttpDelete httpDelete = new HttpDelete(uri);
+        executeMethod(httpDelete, response);
+    }
+
+    private HttpEntity parseToEntity(HttpServletRequest request) throws IOException {
+        List<NameValuePair> nameValuePairs = dealParams(request);
+        HttpEntity httpEntity = new UrlEncodedFormEntity(nameValuePairs);
+        return httpEntity;
+    }
+
+    private String parseToUrl(HttpServletRequest request) {
+        String url = request.getParameter("url");
+        StringBuilder stringBuilder = new StringBuilder(BASE_URL + url);
+        List<NameValuePair> nameValuePairs = dealParams(request);
+        if (nameValuePairs.size() > 0) {
+            stringBuilder.append("?");
+        }
+        for (NameValuePair nameValuePair : nameValuePairs) {
+            stringBuilder.append(nameValuePair.getName()).append("=").append(nameValuePair.getValue()).append("&");
+        }
+        if (nameValuePairs.size() > 0) {
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        }
+        String uri = stringBuilder.toString();
+        System.out.println(uri);
+        return uri;
+    }
+
+    private void executeMethod(HttpUriRequest httpUriRequest, HttpServletResponse response) throws IOException {
+        Map<String, Object> result = new HashMap<String, Object>();
+        CloseableHttpResponse httpResponse = (CloseableHttpResponse) httpClient.execute(httpUriRequest, context);
         if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
             result.put("success", true);
             result.put("data", EntityUtils.toString(httpResponse.getEntity()));
@@ -102,5 +137,15 @@ public class Proxy extends HttpServlet {
         objectMapper.writeValue(response.getOutputStream(), result);
     }
 
-
+    private List<NameValuePair> dealParams(HttpServletRequest request) {
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        Enumeration enumeration = request.getParameterNames();
+        while (enumeration.hasMoreElements()) {
+            String name = enumeration.nextElement().toString();
+            String value = request.getParameter(name);
+            NameValuePair nameValuePair = new BasicNameValuePair(name, value);
+            nameValuePairs.add(nameValuePair);
+        }
+        return nameValuePairs;
+    }
 }
