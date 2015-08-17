a86e8499ef8df955c1b7c0e56c9886c96d741596
ligson <593949938@qq.com>
2015-08-17 07:15:51 +0000

    资源管理

---
+++ src/main/webapp/js/admin/rmgrbase.js
@@ -0,0 +1,22 @@
+/**
+ * Created by ligson on 2015/8/17.
+ */
+function addCategory() {
+    //var tree = $("#categoryTree");
+    //var selectNode = tree.tree("getSelected");
+    $("#addCategoryDlg").dialog("open");
+}
+function showContextMenu(e, node) {
+    e.preventDefault();
+    $("#treeContextMenu").menu("show", {
+        left: e.pageX,
+        top: e.pageY
+    });
+}
+$(function () {
+    $("#categoryTree").tree({
+        onContextMenu: function (e, node) {
+            showContextMenu(e, node);
+        }
+    });
+});
