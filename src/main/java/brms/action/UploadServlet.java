c903b3fd37ec6d41650e3ce2b9378738c2c5374e
ligson <593949938@qq.com>
2015-09-18 09:59:27 +0800

    结构类型词汇表

--- src/main/java/brms/action/UploadServlet.java
+++ src/main/java/brms/action/UploadServlet.java
@@ -6,6 +6,7 @@ import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.FileUtils;
+import org.json.JSONObject;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,22 +14,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by ligson on 2015/8/12.
  */
 public class UploadServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
         request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
         DiskFileItemFactory fileItemFactory = new DiskFileItemFactory();
         ServletFileUpload upload = new ServletFileUpload(fileItemFactory);
         try {
             List<FileItem> fileItems = upload.parseRequest(request);
             System.out.println(fileItems.size());
-            if(fileItems.size()>0){
+            if (fileItems.size() > 0) {
                 FileItem fileItem = fileItems.iterator().next();
-                FileUtils.copyInputStreamToFile(fileItem.getInputStream(), new File(fileItem.getFieldName()));
+                File dest = new File(fileItem.getName());
+                FileUtils.copyInputStreamToFile(fileItem.getInputStream(), dest);
+                Map<String, Object> res = new HashMap<>();
+                res.put("file", dest.getAbsolutePath());
+                JSONObject jsonObject = new JSONObject(res);
+
+                response.getWriter().print(jsonObject.toString());
             }
         } catch (FileUploadException e) {
             e.printStackTrace();
@@ -36,6 +47,6 @@ public class UploadServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doPost(request, response);
     }
 }
---
+++ src/main/webapp/test/upload.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2015/9/18
+  Time: 8:05
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>xxxx</title>
+</head>
+<body>
+<form action="/bcms/upload" method="post" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <input type="submit" value="submit">
+</form>
+</body>
+</html>
